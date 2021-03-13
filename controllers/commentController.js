022e22bd702e8a52ebc5be4a779a5225bfd67502
gywreb <longnguyenhoang91@gmail.com>
2021-03-13 20:24:37 +0700

    update comment api

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,28 +4,28 @@ const User = require("../database/models/User");
 const { asyncMiddleware } = require("../middleware/asyncMiddleware");
 const { SuccessResponse } = require("../models/SuccessResponse");
 
-exports.createComment = asyncMiddleware(async (req, res, next) => {
-  const { content, postId } = req.body;
-  const newComment = new Comment({
-    content,
-    author: req.user._doc._id,
-    post: postId,
-  });
+// exports.createComment = asyncMiddleware(async (req, res, next) => {
+//   const { content, postId } = req.body;
+//   const newComment = new Comment({
+//     content,
+//     author: req.user._doc._id,
+//     post: postId,
+//   });
 
-  const comment = await newComment.save();
+//   const comment = await newComment.save();
 
-  await User.findByIdAndUpdate(
-    { _id: req.user._doc._id },
-    { $push: { comment: comment._id } }
-  );
+//   await User.findByIdAndUpdate(
+//     { _id: req.user._doc._id },
+//     { $push: { comment: comment._id } }
+//   );
 
-  await Post.findByIdAndUpdate(
-    { _id: postId },
-    { $push: { comment: comment._id } }
-  );
+//   await Post.findByIdAndUpdate(
+//     { _id: postId },
+//     { $push: { comment: comment._id } }
+//   );
 
-  res.status(200).json(new SuccessResponse(200, comment));
-});
+//   res.status(200).json(new SuccessResponse(200, comment));
+// });
 
 const updateComment = asyncMiddleware(async (req, res, next) => {
   const { newContent } = req.body;
